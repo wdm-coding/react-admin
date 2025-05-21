569a96f16d72977a928d4c1debed588505121b1a
wdm-coding <wdmcoding0620@163.com>
2025-05-21 15:12:41 +0800

    微调

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,1 +1,14 @@
-console.log('api!')
+import request from '@/utils/request.ts'
+// 定义后端响应数据的类型
+interface Response {
+  code: number;
+  data: any; 
+  msg: string;
+}
+// 查询日志列表
+export function getSSE():Promise<Response> {
+	return request({
+		url: '/sse/send',
+		method: 'get'
+	})
+}
--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,7 +1,24 @@
-
+import {getSSE} from '@/api/index'
+import { Button } from 'antd'
 function Home() {
+	const initSSE = () =>{
+		const eventSource = new EventSource('/api/sse/message') // 创建EventSource对象，连接到服务器端的事件流
+		eventSource.onmessage = function(event) {
+			const {data} = event
+			console.log('接收到的数据:', JSON.parse(data)) // 接收到来自服务器的数据时执行的操作
+		}
+		eventSource.onerror = () => {
+			eventSource.close()
+		}
+	}
+	initSSE()
+	const sendHandler = () => {
+		getSSE()
+	}
 	return (
-		<div className="w-[40vw] flex justify-around" />
+		<div className="w-[40vw] flex justify-around">
+			<Button onClick={sendHandler}>发送</Button>
+		</div>
 	)
 }
 
