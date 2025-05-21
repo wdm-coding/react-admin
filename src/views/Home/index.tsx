293f52ef8c50aab8af02c1c3321e32d993477e83
wdm-coding <wdmcoding0620@163.com>
2025-05-21 17:56:43 +0800

    微调

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -3,12 +3,15 @@ import { Button } from 'antd'
 function Home() {
 	const initSSE = () =>{
 		const eventSource = new EventSource('/api/sse/message') // 创建EventSource对象，连接到服务器端的事件流
+		eventSource.onopen = () => {
+			console.log('连接成功')
+		}
 		eventSource.onmessage = function(event) {
 			const {data} = event
 			console.log('接收到的数据:', JSON.parse(data)) // 接收到来自服务器的数据时执行的操作
 		}
-		eventSource.onerror = () => {
-			eventSource.close()
+		eventSource.onerror = e => {
+			console.log('连接错误:', e)
 		}
 	}
 	initSSE()
